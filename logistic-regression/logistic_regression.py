b6913d821e7790e1dcb6d3a49b6a53f65f29846c
Suraj Verma <vermasuraj008@gmail.com>
2024-03-05 22:33:15 +0530

    update

--- logistic-regression/logistic_regression.py
+++ logistic-regression/logistic_regression.py
@@ -77,7 +77,3 @@ plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
-
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)), 1))
-print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
